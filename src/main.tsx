461ccfb046bd727bf6606cccc7f70e37ef29e01e
daichi2mori <daichi2mori@gmail.com>
2024-08-18 15:22:15 +0900

    fix: add reset css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "@unocss/reset/tailwind.css";
 import "virtual:uno.css";
 
 const rootElement = document.getElementById("root");
